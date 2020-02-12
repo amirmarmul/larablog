d19a32178f248df022f5e0cd4a854f33decff62b
amirmarmul <amiruddinmarmul@gmail.com>
2020-02-12 17:15:45 +0700

    update home page

--- resources/views/front/home/index.blade.php
+++ resources/views/front/home/index.blade.php
@@ -3,11 +3,16 @@
 @section('title', 'Home')
 
 @section('content')
-<div class="flex-center position-ref full-height">
-    <div class="content">
-        <div class="title m-b-md">
-            {{ config('app.name') }}
-        </div>
+<div class="jumbotron jumbotron-fluid">
+    <div class="container">
+        <h1 class="display-4 font-italic">Your Name's personal site</h1>
+        <p class="lead my-3">stay hungry, stay creative</p>
     </div>
 </div>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo aliquam, deleniti laboriosam, debitis minima id consequuntur minus quo vitae quidem praesentium eaque molestias quas nobis totam aperiam error fugiat accusamus?
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum officia harum quae eligendi repellat perferendis voluptatum eius debitis, quis ab libero, eveniet distinctio magni. Architecto exercitationem incidunt cupiditate provident nostrum?
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse deleniti, eaque, molestias eveniet eius soluta dolor neque earum deserunt, repellendus a illo culpa odio quas ducimus quisquam at repudiandae? Ipsam?</p>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, ex! Tenetur, doloremque. Expedita quis vitae, quasi consectetur sunt minus adipisci rerum? Dolores quos fugiat illum totam quo laudantium optio suscipit!</p>
 @endsection
