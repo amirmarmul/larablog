29798c4ff7bc58424cd7228c01a346b0bad645a9
amirmarmul <amiruddinmarmul@gmail.com>
2020-02-12 17:28:49 +0700

    add simple metric

--- resources/views/back/dashboard/index.blade.php
+++ resources/views/back/dashboard/index.blade.php
@@ -1,7 +1,42 @@
 @extends('back.layouts.app')
 
 @section('content')
-<h3>Dashboard</h3>
+<div class="row">
+    <div class="col-md-3">
+        <div class="card">
+            <div class="card-header">Users</div>
+            <div class="card-body">
+                <h4 class="display-4">{{ App\User::count() }}</h4>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="card">
+            <div class="card-header">Posts</div>
+            <div class="card-body">
+                <h4 class="display-4">{{ App\Post::count() }}</h4>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="card">
+            <div class="card-header">Tags</div>
+            <div class="card-body">
+                <h4 class="display-4">{{ App\Tag::count() }}</h4>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="card">
+            <div class="card-header">Blank</div>
+            <div class="card-body">
+                <h4 class="display-4">-</h4>
+            </div>
+        </div>
+    </div>
+</div>
+<br>
+
 <div class="card">
     <div class="card-header">Welcome, {{ Auth::user()->name }}</div>
 
